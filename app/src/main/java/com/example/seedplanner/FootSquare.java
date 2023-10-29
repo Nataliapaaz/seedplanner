a1b4d651f4cbdef441029d8a960a4f865ed272ef
Natalia Paredes <natalia.paz.paredes@hotmail.com>
2023-10-29 19:15:55 -0300

    Implementando el Foot Square por fin funciona mas o menos, gracias Diosito!

--- app/src/main/java/com/example/seedplanner/FootSquare.java
+++ app/src/main/java/com/example/seedplanner/FootSquare.java
@@ -7,58 +7,60 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FootSquare#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FootSquare extends Fragment {
+    private Spinner opciones;
+    private ImageView imageView;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FootSquare() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FootSquare.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FootSquare newInstance(String param1, String param2) {
-        FootSquare fragment = new FootSquare();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private Button verButton;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_foot_square, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_foot_square, container, false);
+        opciones = view.findViewById(R.id.opciones);
+        imageView = view.findViewById(R.id.imageView4);
+        verButton = view.findViewById(R.id.ver);
+
+        opciones.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Opción del Spinner
+                String opcionSeleccionada = opciones.getSelectedItem().toString();
+
+                // Cambiar la imagen cuando se selecciona una opción
+                if (opcionSeleccionada.equals("Verdura")) {
+                    imageView.setImageResource(R.drawable.verduras);
+                } else if (opcionSeleccionada.equals("Hierbas")) {
+                    imageView.setImageResource(R.drawable.hierbas);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // errores
+            }
+        });
+
+        verButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // opción seleccionada
+                String opcionSeleccionada = opciones.getSelectedItem().toString();
+
+                // Cambiar la imagen
+                if (opcionSeleccionada.equals("Verdura")) {
+                    imageView.setImageResource(R.drawable.verduras);
+                } else if (opcionSeleccionada.equals("Hierbas")) {
+                    imageView.setImageResource(R.drawable.hierbas);
+                }
+            }
+        });
+
+        return view;
     }
 }
